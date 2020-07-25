df9ec2c3b7d6fc6db775300c711faaa015f8cf40
Swerve <james.goldsworthy@gmail.com>
2020-07-25 16:36:00 +0100

    Saturday Navigation updates

--- assets/js/app-min.js
+++ assets/js/app-min.js
@@ -1,1 +1,1 @@
-$((function(){$("#nav-toggle").click((function(){$("#content").toggleClass("pushed")}))}));
+$((function(){$("#nav-toggle").click((function(){$("#content").toggleClass("pushed")})),$("#food").click((function(){$(this).toggleClass("open-sub"),$("#drink").removeClass("open-sub")})),$("#drink").click((function(){$(this).toggleClass("open-sub"),$("#food").removeClass("open-sub")}))}));
--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,16 @@ $('#nav-toggle').click(function() {
  $('#content').toggleClass('pushed');
 });
 
+$('#food').click(function() {
+	$(this).toggleClass('open-sub');
+	$('#drink').removeClass('open-sub')
+});
+
+$('#drink').click(function() {
+	$(this).toggleClass('open-sub');
+	$('#food').removeClass('open-sub')
+});
+
 	
 	
 });
